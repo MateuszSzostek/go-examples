a8217cbec264bbe11fdfd07aedae19c9505427ec
MateuszSzostek <szostekm230590@gmail.com>
2023-12-17 23:55:14 +0100

    Begin of teaching variables in go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nameOne string = "Example string"
+	var nameTwo = "smfmafo"
+	var nameThree string
+
+	fmt.Println(nameOne)
+
+	fmt.Println("Hello World", nameTwo, nameThree)
+
+	nameOne = "sgsdg"
+
+	nameFour := "strinfdasdfa"
+
+	fmt.Println(nameFour)
+
+	var intOne int = 20
+	var intTwo = 20
+	intThree := 40
+
+	fmt.Println(intOne,intTwo, intThree)
+
+	var numOne int8 = 25
+	var uintValue uint8 = 255
+
+	var oneScore float32 = 0.234243654
+	var twoScore float64 = -23462.2346263462
+
+	fmt.Println(numOne, uintValue, oneScore, twoScore)
+}
